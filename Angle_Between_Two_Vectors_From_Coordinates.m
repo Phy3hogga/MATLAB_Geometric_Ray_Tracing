79f8fde4c72f79bba78cc9e65a4aed8988327928
Phy3hogga <alexander.hogg@ntu.ac.uk>
2024-06-11 14:43:49 +0100

    Angle_Between_Two_Vectors_From_Coordinates.m - Added input validation and automatic table resizing

--- Angle_Between_Two_Vectors_From_Coordinates.m
+++ Angle_Between_Two_Vectors_From_Coordinates.m
@@ -1,5 +1,66 @@
 %% Calculates the dot product given a single start and end coordinate, for a series of common coordinates
 function [Angle] = Angle_Between_Two_Vectors_From_Coordinates(Origin, Destination, Common_Central_Coordinate_List)
+    %% Pre-handling to verify vector sizing is compatible; or handle automatic duplication of array elements
+    if(height(Common_Central_Coordinate_List) == height(Origin))
+        Length_Match_Common_Origin = true;
+    else
+        Length_Match_Common_Origin = false;
+    end
+    if(height(Common_Central_Coordinate_List) == height(Destination))
+        Length_Match_Common_Destination = true;
+    else
+        Length_Match_Common_Destination = false;
+    end
+    if(height(Origin) == height(Destination))
+        Length_Match_Origin_Destination = true;
+    else
+        Length_Match_Origin_Destination = false;
+    end
+
+    if(Length_Match_Common_Origin && Length_Match_Common_Destination && Length_Match_Origin_Destination)
+       %Valid -> all lengths match
+    else
+        if(sum([Length_Match_Common_Origin, Length_Match_Common_Destination, Length_Match_Origin_Destination]) == 1)
+            %If only a single length match occurs
+            if(Length_Match_Common_Origin)
+                if(height(Destination) == 1)
+                    Destination = repmat(Destination, height(Common_Central_Coordinate_List), 1);
+                elseif(height(Origin) == 1)
+                    Origin = repmat(Origin, height(Destination), 1);
+                    Common_Central_Coordinate_List = repmat(Common_Central_Coordinate_List, height(Destination), 1);
+                else
+                    %Invalid; no matching vector lengths
+                    error("Angle_Between_Two_Vectors_From_Coordinates : Requires a minimum of a pair of matching vector lengths.");
+                end
+            end
+            if(Length_Match_Common_Destination)
+                if(height(Origin) == 1)
+                    Origin = repmat(Origin, height(Common_Central_Coordinate_List), 1);
+                elseif(height(Destination) == 1)
+                    Destination = repmat(Destination, height(Origin), 1);
+                    Common_Central_Coordinate_List = repmat(Common_Central_Coordinate_List, height(Origin), 1);
+                else
+                    %Invalid; no matching vector lengths
+                    error("Angle_Between_Two_Vectors_From_Coordinates : Requires a minimum of a pair of matching vector lengths.");
+                end
+            end
+            if(Length_Match_Origin_Destination)
+                if(height(Common_Central_Coordinate_List) == 1)
+                    Common_Central_Coordinate_List = repmat(Common_Central_Coordinate_List, height(Origin), 1);
+                elseif(height(Origin) == 1)
+                    Origin = repmat(Origin, height(Common_Central_Coordinate_List), 1);
+                    Destination = repmat(Destination, height(Common_Central_Coordinate_List), 1);
+                else
+                    %Invalid; no matching vector lengths
+                    error("Angle_Between_Two_Vectors_From_Coordinates : Requires a minimum of a pair of matching vector lengths.");
+                end
+            end
+        else
+            %Invalid; no matching vector lengths
+            error("Angle_Between_Two_Vectors_From_Coordinates : Requires a minimum of a pair of matching vector lengths.");
+        end
+    end
+    
     %Generate vectors from coordinate lists
     [Vector_A, ~] = Vector_From_3D_Coordinate_Pair(Origin, Common_Central_Coordinate_List);
     [Vector_B, ~] = Vector_From_3D_Coordinate_Pair(Common_Central_Coordinate_List, Destination);
